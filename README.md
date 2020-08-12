e196ac0e1718315dac4b619c52d0d9578b499dd0
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2020-08-11 20:25:35 -0700

    Connected styles in the server, basic nav renders

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Bootstrap Project: Seattle Meows
+
+### Author: Krystal Madrinan
+
+### Links and Resouces
+
+- [front-end application](https://seattle-meows.herokuapp.com/)
--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 'use strict';
 
-// Load environment variables from the .env
+// Load environment variables 
 require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use(express.static(__dirname + '/public'));
 
 
 //ROUTES
 
-app.get('/', renderHomePage);
+app.get('/', renderHome);
+app.get('/resources', renderResources);
+app.get('/blog', renderBlog);
+app.get('/contactus', renderContact);
 
 
 
-function renderHomePage(request, response) {
-    response.sendFile(__dirname + '/views/index.html');
+function renderHome(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 }
 
+function renderResources(req, res) {
+    res.sendFile(__dirname + '/views/resources.html');
+}
+
+function renderBlog(req, res) {
+    res.sendFile(__dirname + '/views/blog.html');
+}
+
+function renderContact(req, res) {
+    res.sendFile(__dirname + '/views/contact.html');
+}
 
 
 
